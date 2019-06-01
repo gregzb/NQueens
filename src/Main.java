7c74b5e49323bed921f1b5dfd914a1d1847dd62c
Greg Z <greg.zbo3@gmail.com>
2019-06-01 18:44:52 -0400

    added time

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,14 @@ import java.util.Arrays;
 
 public class Main {
 
-    static ArrayList<Location> directions;
-    int size = 8;
+    int size = 12;
 
     public static void main(String[] args) {
         new Main().run();
     }
 
     public int[][] copyBoard(int[][] board) {
-        int [][] temp = new int[board.length][];
+        int [][] temp = board.clone();
         for(int i = 0; i < board.length; i++)
             temp[i] = board[i].clone();
         return temp;
@@ -29,17 +28,28 @@ public class Main {
 
     public void run() {
 
-        int[][] board = new int[size][size];
-        //System.out.println(nQueens(board, size));
-        System.out.println(nQueens(board, size-1));
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < 1; i++) {
+
+            int[][] board = new int[size][size];
+            //System.out.println(nQueens(board, size));
+            System.out.println(nQueens(board, size - 1));
+        }
+
+        long endTime = System.nanoTime();
+
+        double time = (endTime - startTime) * Math.pow(10, -9);
+        System.out.println("Took " + time + " seconds");
     }
 
     public int nQueens(int[][] board, int row) {
         if (row < 0) {
-            System.out.println("");
-            printBoard(board);
+            //System.out.println("");
+            //printBoard(board);
             return 1;
         }
+
         int total = 0;
         for (int col = 0; col < size; col++) {
             int[][] boardCopy = copyBoard(board);
@@ -75,81 +85,5 @@ public class Main {
             }
         }
         return total;
-    }
-
-    public int nQueens2(int[][] board, int remaining) {
-        if (remaining == 0) {
-            //System.out.println("found one");
-            System.out.println();
-            printBoard(board);
-            return 1;
-        }
-        //int yeet = true;
-        int total = 0;
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-                int[][] boardCopy = copyBoard(board);
-//                System.out.println();
-//                System.out.println("Remaining: " + remaining);
-//                System.out.println("row: " + row + ", col: " + col);
-//                printBoard(boardCopy);
-                if (boardCopy[row][col] == 0) {
-
-                    for (int iRow = 0; iRow < size; iRow++) {
-                        boardCopy[iRow][col] = 1;
-                    } //goes down row
-                    for (int iCol = 0; iCol < size; iCol++) {
-                        boardCopy[row][iCol] = 1;
-                    } //goes down col
-
-                    int minVal = Math.min(row, col);
-                    int otherMinVal = Math.min(row, size-col-1);
-
-                    //top left diagonal
-                    for (int iRow = row-minVal, iCol = col-minVal; iRow < size && iCol < size; iRow++, iCol++) {
-                        boardCopy[iRow][iCol] = 1;
-                    }
-
-                    //top right diagonal
-                    for (int iRow = row-otherMinVal, iCol = col+otherMinVal; iRow < size && iCol >= 0; iRow++, iCol--) {
-                        boardCopy[iRow][iCol] = 1;
-                    }
-
-                    boardCopy[row][col] = 2;
-
-                    total += nQueens(boardCopy, remaining-1);
-                }
-            }
-        }
-        return total;
-    }
-
-    static class Location {
-        public final int x, y;
-        public Location(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public Location(Location l) {
-            this.x = l.x;
-            this.y = l.y;
-        }
-
-        public Location add(Location l) {
-            return new Location(x+l.x, y+l.y);
-        }
-
-        public String toString() {
-            return "(" + x + ", " + y + ")";
-        }
-
-        public boolean equals(Object o) {
-            if (!(o instanceof Location)) {
-                return false;
-            }
-            Location l = (Location) o;
-            return x == l.x && y == l.y;
-        }
     }
 }
---
+++ src/QueenBoard.java
@@ -0,0 +1,166 @@
+public class QueenBoard{
+    private int[][] board;
+    public QueenBoard(int size){
+        board=new int[size][size];
+        //set the square board's size
+        this.clear();
+        //make everything zero
+    }
+    private void clear(){
+        for (int i=0;i<board.length;i++){
+            for (int j=0;j<board.length;j++){
+                board[i][j]=0;
+            }
+        }
+    }
+    //private boolean addQueen(int r,int c){}
+    //private boolean removeQueen(int r,int c){}
+    public String toString(){
+        String s="";
+        for (int i=0;i<board.length;i++){
+            s+="\n";
+            for (int j=0;j<board.length;j++){
+                if (board[i][j]!=-1){
+                    s+="_ ";
+                }
+                else{
+                    s+="Q ";
+                }
+            }
+            s=s.substring(0,s.length()-1);
+        }
+        return s;
+    }
+    public boolean solve(){
+        for(int i=0;i<board.length;i++){
+            for(int h=0;h<board.length;h++){
+                if (board[i][h]!=0){
+                    throw new IllegalStateException();
+                    //if states do not begin with all zeros, throw error
+                }
+            }
+        }
+        if (solution(0)){
+            return true;
+        }
+        else{
+            this.clear();
+            //if no solution, clear it first
+            return false;
+        }
+    }
+    public boolean solution(int r){
+        if(r==board.length){
+            return true;
+        }
+        for (int i=0;i<board.length;i++){
+            if (addQueen(r,i)){
+                if (solution(r+1)){
+                    return true;
+                }
+            }
+            removeQueen(r,i);
+        }
+        return false;
+    }
+    private boolean addQueen(int r,int c){
+        if (board[r][c]!=0){
+            return false;
+        }
+        for (int i=0;i<board.length;i++){
+            board[r][i]=board[r][i]+1;
+            if(r!=c||(r!=i)){
+                board[i][c]=board[i][c]+1;
+            }
+        }
+        for (int i=0;i<board.length;i++){
+            if(i+r<board.length&&i+c<board.length){
+                board[r+i][c+i]=board[r+i][c+i]+1;
+            }
+            if(r-i>=0&&c-i>=0){
+                board[r-i][c-i]=board[r-i][c-i]+1;
+            }
+            if(i+r<board.length&&c-i>=0){
+                board[r+i][c-i]=board[r+i][c-i]+1;
+            }
+            if(r-i>=0&&i+c<board.length){
+                board[r-i][c+i]=board[r-i][c+i]+1;
+            }
+        }
+        board[r][c]=-1;
+        return true;
+    }
+    private boolean removeQueen(int r,int c){
+        if (board[r][c]!=-1){
+            return false;
+        }
+        for (int i=0;i<board.length;i++){
+            board[r][i]=board[r][i]-1;
+            if(r!=c||(r!=i)){
+                board[i][c]=board[i][c]-1;
+            }
+        }
+        for (int i=0;i<board.length;i++){
+            if(i+r<board.length&&i+c<board.length){
+                board[r+i][c+i]=board[r+i][c+i]-1;
+            }
+            if(r-i>=0&&c-i>=0){
+                board[r-i][c-i]=board[r-i][c-i]-1;
+            }
+            if(i+r<board.length&&c-i>=0){
+                board[r+i][c-i]=board[r+i][c-i]-1;
+            }
+            if(r-i>=0&&i+c<board.length){
+                board[r-i][c+i]=board[r-i][c+i]-1;
+            }
+        }
+        board[r][c]=0;
+        return true;
+    }
+    public int countSolutions(){
+        for(int i=0;i<board.length;i++){
+            for(int h=0;h<board.length;h++){
+                if (board[i][h]!=0){
+                    throw new IllegalStateException();
+                }
+            }
+        }
+        int answer=countingSol(0);
+        this.clear();
+        return answer;
+    }
+    public int countingSol(int r){
+        if(r==board.length){
+            return 1;
+        }
+        int count=0;
+        for (int i=0;i<board.length;i++){
+            if (addQueen(r,i)){
+                count+=countingSol(r+1);
+                removeQueen(r,i);
+            }
+        }
+        return count;
+    }
+    public static void runTest(int i){
+        QueenBoard b;
+        int[]tests =   {1,2,3,4,5,8,13};
+        int[]answers = {1,0,0,2,10,92,724};
+        if(i >= 0 && i < tests.length ){
+            int size = tests[i];
+            int correct = answers[i];
+            b = new QueenBoard(size);
+            int ans  = b.countSolutions();
+
+            if(correct==ans){
+                System.out.println("PASS board size: "+tests[i]+" "+ans);
+            }else{
+                System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        runTest(6);
+    }
+}
